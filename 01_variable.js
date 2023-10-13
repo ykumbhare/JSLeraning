5863c33603c18879d991a6870a1601230d719edb
ykumbhare <ykumbhare268@gmail.com>
2023-10-13 12:43:02 +0000

    code01
    01

---
+++ 01_variable.js
@@ -0,0 +1,17 @@
+
+var firstName;// variable declaration
+firstName = "Yogesh"; // variable initialization
+console.log(firstName);
+firstName = "Mr. Yogesh"
+console.log(firstName); // Variable update
+
+var lastName = "Kumbhare" // variable declaration and initialization
+console.log(lastName);
+
+var height; // variable initialization
+height = "6.2 feet"
+console.log(height);
+
+var marks = '78%'
+console.log("10th Marks",marks, "Grade A");
+
